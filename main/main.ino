698635b0604878429e5b7fcdbddaa88ca4a01fb5
erwanmq <phenix60@outlook.com>
2025-11-10 14:05:23 +0100

    Add error handling.

--- main/main.ino
+++ main/main.ino
@@ -23,6 +23,26 @@
 #define ERASE_BLK_3 0x80
 #define ERASE_BLK_4 0xC0
 
+enum class Error
+{
+  OK,
+  TIMEOUT_ERR,
+  CHECKSUM_ERR,
+  SECURITY_ERR,
+  ERR,
+};
+
+#define CHECK_ERROR(x, msg) \
+do { \
+  if (Error::OK != x) \
+  { \
+    char buf[64]; \
+    sprintf(buf, "%s Error nb: %d", msg, x); \
+    Serial.println(buf); \
+    exit(0); \
+  } \
+} while (0)
+
 const byte RST_PIN  = 4;
 const byte PSEN_PIN = 6;
 const byte TX_PIN   = 10;
@@ -56,17 +76,55 @@ void enterBootloader()
   delay_without_stop(DELAY_FOR_STABILITY);
 }
 
-bool write_to_mcu(const byte *bytes, uint8_t size)
+Error read_mcu_serial()
 {
+  Error err = Error::ERR;
+  while (mcuSerial.available()) {
+    byte b = mcuSerial.read();
+    if (b < 0x20 || b > 0x7E) {
+        Serial.print("0x");
+        if (b < 0x10) Serial.print('0');
+        Serial.print(b, HEX);
+        Serial.print(" ");
+    } else {
+        Serial.print((char)b);
+    }
+
+    switch (b)
+    {
+      case 'X':
+        err = Error::CHECKSUM_ERR;
+        break;
+      
+      case 'P':
+        err = Error::SECURITY_ERR;
+        break;
+      
+      case '.':
+        err = Error::OK;
+        break;
+      
+      default: {}
+    }
+
+  }
+  Serial.println();
+
+  return err;
+}
+
+Error write_to_mcu(const byte *bytes, uint8_t size)
+{
+  Error err = Error::OK;
   mcuSerial.write(bytes, size);
   unsigned long start = micros();
   while((!mcuSerial.available()) && (micros() - start < TIMER_LOOP));
 
   if (!mcuSerial.available())
   {
-    return false;
+    err = Error::TIMEOUT_ERR;
   }
-  return true;
+  return err;
 }
 
 void byte_to_hex(uint8_t byte, char *out) {
@@ -75,7 +133,7 @@ void byte_to_hex(uint8_t byte, char *out) {
     out[1] = hex_chars[byte & 0x0F];
 }
 
-bool create_frame_header_and_write(const byte *bytes, uint8_t size)
+Error create_frame_header_and_write(const byte *bytes, uint8_t size)
 {
   const byte record_mark = ':';
   byte load_offset[2] = { 0x00, 0x00 };
@@ -106,41 +164,30 @@ bool create_frame_header_and_write(const byte *bytes, uint8_t size)
 
   byte_to_hex(checksum, &bytes_with_frame[offset]); offset += 2;
 
-  return write_to_mcu(bytes_with_frame, offset);
+  Error err = write_to_mcu(bytes_with_frame, offset);
+  if (Error::OK != err)
+  {
+    return err;
+  }
+  return read_mcu_serial();
 }
 
-bool initialize_baudrate()
+Error initialize_baudrate()
 {
   byte data = 'U';
   return write_to_mcu(&data, 1); // Don't need a frame for the auto baudrate
 }
 
-bool read_mcu_id()
+Error read_mcu_id()
 {
   const byte data[] = { READ_FCT, READ_IDs, READ_MANUFACTURER_ID };
   return create_frame_header_and_write(data, sizeof(data));
 }
 
-bool erase_blocks(int block)
+Error erase_blocks(int block)
 {
   byte data[] = { WRITE_FCT, ERASE, block };
   return create_frame_header_and_write(data, sizeof(data));
-}
-
-void read_mcu_serial()
-{
-  while (mcuSerial.available()) {
-    byte b = mcuSerial.read();
-    if (b < 0x20 || b > 0x7E) {
-        Serial.print("0x");
-        if (b < 0x10) Serial.print('0');
-        Serial.print(b, HEX);
-        Serial.print(" ");
-    } else {
-        Serial.print((char)b);
-    }
-  }
-  Serial.println();
 }
 
 void setup ()
@@ -151,24 +198,19 @@ void setup ()
   delay_without_stop(DELAY_BOOTTIME);
   enterBootloader();
 
+  Error err;
+
   /* MCU entered in the bootloader stage. 
   We have to send the character "U" and wait for its response. */
-  if (!initialize_baudrate())
-  {
-    Serial.print("Timeout\n");
-    return; // We stop the program
-  }
-  read_mcu_serial();
+  err = initialize_baudrate();
+  CHECK_ERROR(err, "Initialize baudrate");
 
   /* Check if the MCU is responding to a command. */
-  if (!read_mcu_id())
-  {
-    Serial.print("Timeout\n");
-    return; // We stop the program
-  }
-  read_mcu_serial();
+  err = read_mcu_id();
+  CHECK_ERROR(err, "read_mcu_id");
 
-  erase_blocks(ERASE_BLK_0);
+  err = erase_blocks(ERASE_BLK_0);
+  CHECK_ERROR(err, "erase_blocks");
 }
 
 void loop ()
